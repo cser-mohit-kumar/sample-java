e6cb27943ee8b62a84662f717e40a6520af86fb3
Mohit Kumar <cser.mohit.kumar@gmail.com>
2026-05-20 11:26:12 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// Every Java program must have at least one class
+public class HelloWorld {
+    // The main method is the entry point where the program starts
+    public static void main(String[] args) {
+        // System.out.println prints text followed by a new line to the console
+        System.out.println("Hello, World!");
+    }
+}
+
