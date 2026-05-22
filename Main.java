5cb086aa43477ddc0ea6c90b0e7e3d3bfa59a34a
cser-mohit-kumar <cser-mohit-kumar@gmail.com>
2026-05-22 14:18:52 +0530

    edit main

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     // The main method is the entry point where the program starts
     public static void main(String[] args) {
         // System.out.println prints text followed by a new line to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! 123");
         System.out.println("Hello, World! 123");
     }
 }
